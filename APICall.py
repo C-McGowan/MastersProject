e66a67317c17baa84373a0bae0053030e1b85ddb
C-McGowan <66428473+C-McGowan@users.noreply.github.com>
2020-06-15 18:07:42 +0100

    Update APICall.py

--- APICall.py
+++ APICall.py
@@ -10,16 +10,18 @@ latitudes, longitudes = [54.7678+4*0.0141,54.7678+3*0.0141,54.7678+2*0.0141,54.7
 MOID = 350525
 
 def apiRequest(URL):
+    """Pulls a api request and returns in json format"""
     request = requests.get(URL)
     data = request.json()
     return data
 
 def dataDump(data, target_file):
+    """Takes data and dumps it into a json file"""
     with open(target_file, "w") as data_file:
         json.dump(data, data_file)
 
 def apiForecastRequestDS(latitude,longitude):
-    """Provides forecast data when initialised from Darksky api, dumps into txt from json with unix timestamp"""
+    """Provides forecast data when initialised from Darksky api"""
     url_forecast = f"https://api.darksky.net/forecast/{apiKeyDS}/{latitude},{longitude}?units=si"
     #unix_time = f"{time.time():.0f}"
     #target_file = f"DSForecastData{unix_time}.json"
@@ -27,6 +29,7 @@ def apiForecastRequestDS(latitude,longitude):
     return data
 
 def apiForecastGridDS(latitudes, longitudes):
+    """Creates a grid of size latitudes x longitudes of DarkSky forecasts and dumps into a single timestamped json file"""
     unix_time = f"{time.time():.0f}"
     target_file = f"DSForecastData{unix_time}.json"
     data = []
@@ -36,14 +39,15 @@ def apiForecastGridDS(latitudes, longitudes):
     dataDump(data, target_file)
 
 def apiForecastRequestMO():
-    url_forecast = f"http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/350525?res=3hourly&key={apiKeyMO}"
+    """Pulls a api request from the desired point specified in MOID variable and dumps into json file with timestamp"""
+    url_forecast = f"http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/{MOID}?res=3hourly&key={apiKeyMO}"
     unix_time = f"{time.time():0f}"
     target_file = f"MOForecastData{unix_time}.json"
     data = apiRequest(url_forecast)
     dataDump(data, target_file)
 
 def apiPastRequests(start_time, iterations, latitude, longitude):
-    """Provides hourly weather data from Darksky api, dumps into json with unix timestamp"""
+    """Provides hourly past weather data from Darksky api, dumps into json with unix timestamp"""
     for i in range(iterations):
         time = start_time + i * 86400
         target_file = f"PastData{time}.txt"
@@ -54,5 +58,6 @@ def apiPastRequests(start_time, iterations, latitude, longitude):
 
 
 if __name__ =="__main__":
+    #For use with cron later
     apiForecastGridDS(latitudes, longitudes)
     apiForecastRequestMO()
